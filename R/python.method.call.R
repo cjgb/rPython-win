9f1cee1302ae8c2739d8e2e3f2341515d4202eab
Gil Bellosta, Carlos <cgb@datanalytics.com>
2015-11-15 21:41:05 +0100

    Fixed bug

--- R/python.method.call.R
+++ R/python.method.call.R
@@ -3,6 +3,6 @@
 #########################################################
 
 python.method.call <- function( py.object, py.method, ... ){
-    python.call( paste( py.object, py.method, sep = "." ) )
+    python.call(paste(py.object, py.method, sep = "."), ... )
 }
 
